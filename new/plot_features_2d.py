5f3a25bb9ac1c6678facb318deed34fab6b3375d
Wout Rombouts <woutrombouts18@gmail.com>
2023-10-25 18:44:10 +0200

    Updated feature correlation plot script

--- new/plot_features_2d.py
+++ new/plot_features_2d.py
@@ -17,9 +17,11 @@ parser.add_argument("run_number", type=int)
 args = parser.parse_args()
 run_number = args.run_number
 
+# Combine features with labeled data
 df_features = pd.read_csv(os.path.join(DATA_FEATURES_DIRECTORY, f"run_{run_number:03d}.csv"))
 df_labeled = pd.read_csv(os.path.join(DATA_LABELED_DIRECTORY, f"run_{run_number:03d}.csv"))
 df = pd.merge(df_features, df_labeled, on='transient')
+df.type = df.type.astype("category")
 df.valid = df.valid.astype("category")
 
 # Plot result
--- new/plot_features_correlation.py
+++ new/plot_features_correlation.py
@@ -2,25 +2,34 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import argparse
 
-from config import DATA_FEATURES_DIRECTORY
+from config import DATA_FEATURES_DIRECTORY, DATA_LABELED_DIRECTORY
 
-RUN_NUMBER = 7
+# Initialise argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("run_number", type=int)
+args = parser.parse_args()
+run_number = args.run_number
 
-csv_path = os.path.join(DATA_FEATURES_DIRECTORY, f"run_{RUN_NUMBER:03d}.csv")
-df = pd.read_csv(csv_path)
+# Combine features with labeled data
+df_features = pd.read_csv(os.path.join(DATA_FEATURES_DIRECTORY, f"run_{run_number:03d}.csv"))
+df_labeled = pd.read_csv(os.path.join(DATA_LABELED_DIRECTORY, f"run_{run_number:03d}.csv"))
+df = pd.merge(df_features, df_labeled, on='transient')
+df.transient = df.transient.astype("category")
+df.type = df.type.astype("category")
+df.valid = df.valid.astype("category")
 
-# Ignore non-feature columns
-df = df.drop('transient', axis=1)
-df = df.drop('valid', axis=1)
+# Only retain numeric columns with no NaN values
+df = df.dropna(axis=1)
+df = df.select_dtypes(include="number")
 
 # Calculate the correlation matrix (using Pearson correlation in this example)
 correlation_matrix = df.corr()
 
 # Create a heatmap
-plt.figure(figsize=(100, 100))
 color = plt.get_cmap('coolwarm')
 color.set_bad('black')
 sns.heatmap(correlation_matrix, annot=True, cmap=color, square=True)
 plt.title("Feature Correlation Matrix")
-plt.savefig(f"plots/run_{RUN_NUMBER:03d}_correlation.png")
+plt.savefig(f"plots/run_{run_number:03d}_correlation.png")
