e4b5a421f8badc5968ac338d7fad74f928dc746e
Wout Rombouts <woutrombouts18@gmail.com>
2023-10-25 18:38:18 +0200

    General cleanup (ditched old stuff)

--- new/plot_downsampling.py
+++ new/plot_downsampling.py
@@ -5,27 +5,13 @@ import numpy as np
 import argparse
 from scipy.optimize import curve_fit
 
-from config import DATA_STRUCTURED_DIRECTORY, WINDOW_SIZE, DOWNSAMPLE_FACTOR
-
-
-def moving_average(tran_data, time_data, downsample_factor, window_size):
-    # Apply moving average filter
-    window = np.ones(window_size) / window_size
-    tran_data = np.convolve(tran_data, window, mode="valid")
-
-    # Adjust time data to match length of convoluted output
-    time_data = time_data[(len(window) - 1) :]
-
-    # Downsample time and frequency data
-    time_data = time_data[::downsample_factor]
-    tran_data = tran_data[::downsample_factor]
-
-    return tran_data, time_data
-
-
-def exponential_decay(t, N, λ, c):
-    return (N - c) * np.exp(-λ * t) + c
-
+from config import (
+    DATA_STRUCTURED_DIRECTORY,
+    WINDOW_SIZE,
+    DOWNSAMPLE_FACTOR,
+    PRETRIG_GUARD_SAMPLES,
+)
+from utils import moving_average, exponential_decay
 
 # Initialise argument parser
 parser = argparse.ArgumentParser()
@@ -37,80 +23,114 @@ tran_number = args.tran_number
 
 h5_path = os.path.join(DATA_STRUCTURED_DIRECTORY, f"run_{run_number:03d}.h5")
 with h5py.File(h5_path, "r") as h5file:
+    run_name = f"run_{run_number:03d}"
     tran_name = f"tran_{tran_number:06d}"
 
-    # Get run number and transients from file
-    run_num = h5file["meta"].attrs["run_id"]
-    transients = h5file["sdr_data"]["all"]
-    tran_data = np.array(transients[f"tran_{tran_number:06d}"])
+    # Get transient data from file
+    transient = h5file["sdr_data"]["all"][tran_name]
 
     # Get additional meta data
     fs = h5file["sdr_data"].attrs["sdr_info_fs"]
     len_pretrig = h5file["sdr_data"].attrs["sdr_info_len_pretrig"]
     len_posttrig = h5file["sdr_data"].attrs["sdr_info_len_posttrig"]
     dsp_ntaps = h5file["sdr_data"].attrs["dsp_info_pre_demod_lpf_taps"]
-
-    # Calculate real time from meta data
     event_len = len_pretrig + len_posttrig - dsp_ntaps
-    time_data = np.arange(start=0, stop=event_len / fs, step=1 / fs) - len_pretrig / fs
 
     # Subtract mean baseline frequency from each sample to get delta frequency
     baseline_freq = h5file["sdr_data"]["all"][tran_name].attrs["baseline_freq_mean_hz"]
     baseline_freq_var = h5file["sdr_data"]["all"][tran_name].attrs[
         "baseline_freq_mean_hz"
     ]
-    tran_data = np.subtract(tran_data, baseline_freq)
+
+    # Construct time and frequency arrays
+    tran_time = np.arange(start=0, stop=event_len / fs, step=1 / fs) - len_pretrig / fs
+    tran_freq = np.subtract(np.array(transient), baseline_freq)
+
+    # Construct pre-trigger baseline arrays
+    tran_pretrig_time = tran_time[: len_pretrig - PRETRIG_GUARD_SAMPLES]
+    tran_pretrig_freq = tran_freq[: len_pretrig - PRETRIG_GUARD_SAMPLES]
+
+    # Construct post-trigger arrays
+    tran_posttrig_time = tran_time[len_pretrig:]
+    tran_posttrig_freq = tran_freq[len_pretrig:]
 
     # Downsample data
-    tran_data_processed, time_data_processed = moving_average(
-        tran_data, time_data, DOWNSAMPLE_FACTOR, WINDOW_SIZE
+    tran_freq_ds, tran_time_ds = moving_average(
+        tran_freq, tran_time, DOWNSAMPLE_FACTOR, WINDOW_SIZE
+    )
+    tran_pretrig_freq_ds, tran_pretrig_time_ds = moving_average(
+        tran_pretrig_freq, tran_pretrig_time, DOWNSAMPLE_FACTOR, WINDOW_SIZE
+    )
+    tran_posttrig_freq_ds, tran_posttrig_time_ds = moving_average(
+        tran_posttrig_freq, tran_posttrig_time, DOWNSAMPLE_FACTOR, WINDOW_SIZE
     )
 
     # Try to fit exponential decay
-
-    # TODO find better way to select zero index
-    zero_timestamp = np.where(np.isclose(time_data_processed, 0, atol=1e-5))[0][0]
-    time_data_processed_exp = time_data_processed[zero_timestamp:]
-    tran_data_processed_exp = tran_data_processed[zero_timestamp:]
-
-    print(f"std: {np.std(tran_data_processed[:zero_timestamp])}")
+    features = {}
+    
+    initial_guess = (
+        np.max(tran_posttrig_freq_ds),
+        1000,
+        np.mean(tran_pretrig_freq_ds),
+    )
+    
+    minimum_exp_height = np.mean(tran_pretrig_freq_ds) + 2 * (
+        np.abs(np.max(tran_pretrig_freq_ds)) + np.abs(np.min(tran_pretrig_freq_ds))
+    )
+    boundaries = (
+        [
+            minimum_exp_height,
+            0,
+            -1e6,
+        ],
+        [1e6, 1e6, 1e6],
+    )
 
     try:
         # params: N, λ, c
         # model: (N - c) * np.exp(-λ * t) + c
         params, _ = curve_fit(
             exponential_decay,
-            time_data_processed_exp,
-            tran_data_processed_exp,
-            p0=(
-                np.max(tran_data_processed_exp),
-                10000,
-                np.mean(tran_data_processed[:zero_timestamp]),
-            ),
-            bounds=(
-                [
-                    np.max(tran_data_processed[:zero_timestamp])
-                    + np.abs(np.max(tran_data_processed[:zero_timestamp])),
-                    0,
-                    -1e5,
-                ],
-                [1e6, 1e6, 1e5],
-            ),
+            tran_posttrig_time_ds,
+            tran_posttrig_freq_ds,
+            p0=initial_guess,
+            bounds=boundaries,
         )
-        print(params)
+        features["exp_fit_N"] = params[0]
+        features["exp_fit_λ"] = params[1]
+        features["exp_fit_c"] = params[2]
     except:
-        print("Exponential fit failed")
+        features["exp_fit_N"] = 0
+        features["exp_fit_λ"] = 0
+        features["exp_fit_c"] = 0
+
+    features["std"] = np.std(tran_pretrig_freq_ds)
+        
+    print(features)
+    print(f"min_exp_height: {minimum_exp_height}")
 
     # Plot results
     figure, axis = plt.subplots(1, 1)
 
-    axis.plot(time_data, tran_data, ".")
-    axis.plot(time_data_processed, tran_data_processed, ".-")
+    axis.plot(tran_time, tran_freq, ".")
+    axis.plot(tran_time_ds, tran_freq_ds, ".-")
+
+    axis.axhline(
+        y=minimum_exp_height,
+        color="r",
+        linestyle="--",
+    )
+    
+    axis.axvline(
+        x=tran_pretrig_time[-1],
+        color="lime",
+        linestyle="--",
+    )
 
     try:
         axis.plot(
-            time_data_processed_exp,
-            exponential_decay(time_data_processed_exp, *params),
+            tran_posttrig_time_ds,
+            exponential_decay(tran_posttrig_time_ds, *params),
             "c-",
             linewidth=3,
         )
@@ -119,11 +139,9 @@ with h5py.File(h5_path, "r") as h5file:
 
     axis.set_title(f"Moving Average ({tran_name})")
 
-    min = np.min(tran_data_processed)
-    min = min / 1.05
-    max = np.max(tran_data_processed)
-    max = max * 1.05
-    axis.set_ylim(min, max)
+    max = np.max(tran_freq_ds)
+    max = max * 1.1
+    axis.set_ylim(-5e3, max)
 
     axis.set_xlabel("Time (s)")
     axis.set_ylabel("Delta frequency (Hz)")
@@ -134,4 +152,4 @@ with h5py.File(h5_path, "r") as h5file:
 
     # Set figure size and save
     figure.set_size_inches(13.5, 7.5)
-    plt.savefig(f"plots/downsampling_run_{run_num:03d}_{tran_name}.png")
+    plt.savefig(f"plots/downsampling_{run_name}_{tran_name}.png")
