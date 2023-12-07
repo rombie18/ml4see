50c999abec31866425664e818aa4950f85d917e2
Wout Rombouts <woutrombouts18@gmail.com>
2023-12-07 13:06:43 +0100

    Improved file downloading mechanism

--- new/data_retrieval_download.py
+++ new/data_retrieval_download.py
@@ -23,12 +23,13 @@ Example:
 
 import csv
 import os
-import json
 import logging
-import subprocess
 import argparse
+import concurrent.futures
 
-from config import DATA_DOWNLOAD_DIRECTORY, DATA_SUMMARY_PATH, DOWNLOAD_RETRIES
+import requests
+
+from config import DATA_DOWNLOAD_DIRECTORY, DATA_SUMMARY_PATH
 
 
 def main():
@@ -46,11 +47,6 @@ def main():
     # Initialise argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("run_numbers", metavar="run_number", nargs="*", type=int)
-    parser.add_argument(
-        "--keep",
-        action="store_true",
-        help="Do not delete file if download process fails",
-    )
     args = parser.parse_args()
 
     # Check if directories exist
@@ -72,28 +68,107 @@ def main():
         run_numbers = [f"run_{run_number:03d}" for run_number in args.run_numbers]
         runs = [run for run in runs if run["name"] in run_numbers]
 
-    logging.info(
-        "It can take some time before the downloading actually starts, this is due to server-side limitations."
-    )
-    command = (
-        f"curl --parallel --parallel-immediate --remote-name-all --retry 10 --retry-connrefused --continue-at - "
-        + " ".join([run["url"] for run in runs])
-    )
+    # Start downloading the files in parallel
+    logging.info("Starting file downloads")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                download_file,
+                run["url"],
+                DATA_DOWNLOAD_DIRECTORY,
+                run["url"].split("/")[-1],
+            )
+            for run in runs
+        ]
+        concurrent.futures.wait(futures)
 
-    # Execute curl command
-    for i in range(1, DOWNLOAD_RETRIES):
+
+def download_file(url, working_dir, file_name, attempts=5, chunk_size=1024):
+    """
+    Download a file with the ability to resume if the download is interrupted.
+
+    Parameters:
+    - url (str): The URL of the file to be downloaded.
+    - working_dir (str): The local directory where the file should be saved.
+    - file_name (str): The name of the file to be saved.
+    - attempts (int): The number of attempts to download the file (default is 5).
+    - chunk_size (int): The size of each chunk to download (default is 1 KB).
+    """
+    local_path = os.path.join(working_dir, file_name)
+
+    for attempt in range(1, attempts):
+        logging.info(f"Downloading file {file_name} (attempt {attempt}/{attempts})")
+
         try:
-            # FIXME on curl command failure, doesn't catch error to except block
-            subprocess.run(command.split(), cwd=DATA_DOWNLOAD_DIRECTORY, check=True)
-            break
-        except subprocess.CalledProcessError:
-            if i == DOWNLOAD_RETRIES:
-                logging.exception(f"Downloading failed after {DOWNLOAD_RETRIES} tries.")
+            # Get file size from server
+            with requests.head(url) as response:
+                server_content_length = int(response.headers.get('Content-Length', ''))
+            
+            # Determine if file should be (partially) downloaded or not
+            if os.path.exists(local_path):
+                local_content_length = os.path.getsize(local_path)
+                if local_content_length == server_content_length:
+                    logging.info(f"Full file {file_name} already exists locally, skipping download")
+                    return
+                else:
+                    logging.debug(f"Partial file {file_name} exists locally")
+                    resume_header = {"Range": f"bytes={local_content_length}-"}
             else:
-                logging.warning(f"Downloading interrupted. Retrying... ({i}/{DOWNLOAD_RETRIES})")
+                logging.debug(f"File {file_name} does not exist locally")
+                local_content_length = 0
+                resume_header = {}
+                
+            # Start file download
+            logging.debug(f"Starting download for file {file_name}")
+            with requests.get(url, stream=True, headers=resume_header) as response:
+                response.raise_for_status()
+
+                if (
+                    "Accept-Ranges" not in response.headers
+                    or response.headers["Accept-Ranges"] == "none"
+                ):
+                    if local_content_length != 0:
+                        logging.warning(
+                            "Server does not support partial content. Unable to resume download."
+                        )
+                        
+                    with open(local_path, "wb") as file:
+                        logging.debug(f"Writing file {file_name} to disk")
+                        for chunk in response.iter_content(chunk_size):
+                            file.write(chunk)
+                else:
+                    with open(local_path, "ab") as file:
+                        logging.debug(
+                            f"Resuming file {file_name} from byte {local_content_length}"
+                        )
+                        for chunk in response.iter_content(chunk_size):
+                            file.write(chunk)
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error downloading file {file_name} with message: {e}")
+        else:
+            logging.info(f"Successfully downloaded {file_name}")
+            break
 
 
 def read_csv(file_path):
+    """
+    Reads a CSV file and returns its content as a list of dictionaries.
+
+    Parameters:
+    - file_path (str): The path to the CSV file to be read.
+
+    Returns:
+    list of dict: A list where each element is a dictionary representing a row in the CSV file.
+                  The keys of the dictionaries are the column names (headers), and the values are the
+                  corresponding values from each row.
+
+    Example:
+    >>> file_path = "example.csv"
+    >>> data = read_csv(file_path)
+    >>> print(data)
+    [{'column1': 'value1', 'column2': 'value2', ...}, {'column1': 'value3', 'column2': 'value4', ...}, ...]
+    """
     result_array = []
 
     with open(file_path, "r") as csv_file:
